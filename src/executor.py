f30fe7acfb6a37a1bdc51fdbf92b3eff0a77e405
3uky <hynek.bucek@gmail.com>
2025-03-02 17:58:52 +0100

    replace Task flags related to execution with state; refactoring - naming

--- src/executor.py
+++ src/executor.py
@@ -1,6 +1,7 @@
 import concurrent.futures
 import logging
 import time
+from enum import Enum
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -12,14 +13,13 @@ class DiGraph:
     def __str__(self):
         for node, neighbors in self.nodes.items():
             neighbors_str = ", ".join(map(str, neighbors))
-            print(f"{node} >> {neighbors_str}")
+            print(f"{node} -> {neighbors_str}")
 
     def add_node(self, node):
         if node not in self.nodes:
             self.nodes[node] = set()
 
     def add_edge(self, u, v):
-        # Add a directed edge from node u to node v
         if u not in self.nodes:
             self.add_node(u)
         if v not in self.nodes:
@@ -40,32 +40,45 @@ class DiGraph:
             if node in self.nodes[u]:
                 self.nodes[u].remove(node)
 
-    def output_nodes(self, node):
+    def get_output_nodes(self, node):
         if node in self.nodes:
             return self.nodes[node]
 
-    def input_nodes(self, node):
+    def get_input_nodes(self, node):
         return {u for u in self.nodes if node in self.nodes[u]}
 
-    def nodes_without_input(self):
+    def get_nodes_without_input_edge(self):
         return {node for node in self.nodes if not any(node in self.nodes[u] for u in self.nodes)}
+
+class TaskState(Enum):
+    NOT_STARTED = 0
+    STARTED = 1
+    FINISHED = 2
 
 class Task:
     def __init__(self, callable):
         self.id = callable.__name__
         self.callable = callable
-        self.started = False
-        self.executed = False
+        self.state = TaskState.NOT_STARTED
         self.result = None
 
     def __str__(self):
         return f"{self.id}"
 
-    def execute(self, inputs):
+    def is_not_started(self):
+        return self.state == TaskState.NOT_STARTED
+
+    def is_running(self):
+        return self.state == TaskState.STARTED
+
+    def is_finished(self):
+        return self.state == TaskState.FINISHED
+
+    def execute(self, inputs=None):
         logger.info(f"Task {self.id} STARTED\tinputs: {inputs}")
-        self.started = True
+        self.state = TaskState.STARTED
         self.result = self.callable(inputs)
-        self.executed = True
+        self.state = TaskState.FINISHED
         logger.info(f"Task {self.id} FINISHED")
 
 class Pipeline:
@@ -81,22 +94,25 @@ class Pipeline:
         self.graph.add_edge(node_a, node_b)
 
     def get_dependencies(self, node):
-        return self.graph.input_nodes(node)
+        return self.graph.get_input_nodes(node)
 
     def get_initial_tasks(self):
-        return {t for t in self.graph.nodes_without_input() if not t.executed}
+        return {task for task in self.get_tasks_without_dependencies() if task.is_not_started()}
 
     def get_ready_to_run_tasks(self):
-        return {t for t in self.get_tasks() if not t.started and self.__are_all_required_inputs_available(t)}
+        return {task for task in self.get_tasks() if task.is_not_started() and self.are_all_required_inputs_available(task)}
 
     def get_tasks(self):
         return self.graph.nodes
 
-    def get_required_inputs(self, task):
-        return {dependency.result for dependency in self.get_dependencies(task)}
+    def get_tasks_without_dependencies(self):
+        return self.graph.get_nodes_without_input_edge()
 
-    def __are_all_required_inputs_available(self, task):
-        return all(input.executed for input in self.graph.input_nodes(task))
+    def get_required_inputs(self, task):
+        return {dependent_task.result for dependent_task in self.get_dependencies(task)}
+
+    def are_all_required_inputs_available(self, task):
+        return all(dependent_task.is_finished() for dependent_task in self.get_dependencies(task))
 
 class Executor:
     """Executor that runs the tasks in the DAG respecting the dependencies."""
@@ -104,18 +120,18 @@ class Executor:
         self.futures = set()
         self.executor = concurrent.futures.ThreadPoolExecutor()
 
-    def remove_from_execution(self, finished_task):
-        if finished_task in self.futures:
-            self.futures.remove(finished_task)
-
     def submit_for_execution(self, tasks):
         for task in tasks:
             future = self.executor.submit(task.execute, pipeline.get_required_inputs(task))
             self.futures.add(future)
 
+    def remove_from_execution(self, finished_task):
+        if finished_task in self.futures:
+            self.futures.remove(finished_task)
+
     def execute_submitted_tasks(self):
-        first_done, _ = concurrent.futures.wait(self.futures, return_when=concurrent.futures.FIRST_COMPLETED)
-        return next(iter(first_done)) # wait return set containing exactly one element with FIRST_COMPLETED option
+        done, _ = concurrent.futures.wait(self.futures, return_when=concurrent.futures.FIRST_COMPLETED)
+        return next(iter(done)) # wait return set containing exactly one element with FIRST_COMPLETED option
 
     def is_there_tasks_to_run(self):
         return bool(self.futures)
@@ -131,23 +147,23 @@ class Executor:
 
 
 # Example Tasks
-def task_a(inputs):
+def task_a(*args, **kwargs):
     time.sleep(1)
     return "Data from Task A"
 
-def task_b(inputs):
+def task_b(*args, **kwargs):
     time.sleep(4)
     return "Data from Task B"
 
-def task_c(inputs):
+def task_c(*args, **kwargs):
     time.sleep(1)
     return "Data from Task C"
 
-def task_d(inputs):
+def task_d(*args, **kwargs):
     time.sleep(1)
     return "Data from Task D"
 
-def task_e(inputs):
+def task_e(*args, **kwargs):
     time.sleep(1)
     return "Data from Task E"
 
