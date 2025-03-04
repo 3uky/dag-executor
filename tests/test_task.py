9344f83728c9768353b58fdd2db5d3ed41f7588f
3uky <hynek.bucek@gmail.com>
2025-03-04 11:23:07 +0100

    task unit tests added

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,23 +1,59 @@
 import pytest
+import threading
+import time
 
-from task import *
+from task import Task, TaskState
 
-def callable_example(*args):
-    pass
+
+def sample_task(x):
+    return x * 2
+
+def sample_task_with_delay(x):
+    time.sleep(1)
+    return x * 2
 
 class TestTask:
-    def test_task_state(self):
-        # GIVEN
-        t = Task(callable_example)
+    def test_task_state_transitions(self):
+        # initially, the task is in NOT_STARTED state
+        task = Task(sample_task)
+        assert task.is_not_started()
+        assert not task.is_running()
+        assert not task.is_finished()
 
-        assert(t.is_running(), False)
-        assert(t.is_finished(), False)
+        # after calling execute, the state should change to STARTED and then FINISHED
+        task.execute(5)
 
-    def test_task_in_finished_state_after_execution_finish(self):
-        # GIVEN
-        t = Task(callable_example)
-        t.execute(None)
+        assert not task.is_not_started()
+        assert not task.is_running()
+        assert task.is_finished()
 
-        assert(t.is_running(), False)
-        assert(t.is_finished(), True)
+    def test_task_execution_result(self):
+        task = Task(sample_task)
+        task.execute(5)
+        assert task.result == 10  # 5 * 2 = 10
 
+    def test_task_is_not_started_after_execution(self):
+        task = Task(sample_task)
+        task.execute(5)
+        assert not task.is_not_started()
+
+    def test_task_is_finished_after_execution(self):
+        task = Task(sample_task)
+        task.execute(5)
+        assert task.is_finished()
+
+    def test_task_is_running_during_execution(self):
+        # run the task in a separate thread
+        task = Task(sample_task_with_delay)
+        task_thread = threading.Thread(target=task.execute, args=(5,))
+        task_thread.start()
+
+        # while the task is running, we check if task is in running state
+        assert task.is_running()
+
+        # wait for the task to finish
+        task_thread.join()
+
+        # after task execution is finished, the state should be `FINISHED`
+        assert task.is_finished()
+        assert not task.is_running()
