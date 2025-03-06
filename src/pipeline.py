4fe5b73eb9dfc87886897e75100a7068d6ce8acc
3uky <hynek.bucek@gmail.com>
2025-03-06 12:06:17 +0100

    refactoring pipeline methods shuffle

--- src/pipeline.py
+++ src/pipeline.py
@@ -12,27 +12,8 @@ class Pipeline:
         self.graph.add_node(task)
         return task
 
-    def get_tasks(self):
-        return self.graph.nodes
-
-    def get_ready_to_run_tasks(self):
-        return [task for task in self.get_tasks() if task.is_pending() and self.are_inputs_available(task)]
-
-    def are_inputs_available(self, task):
-        return all(dependent_task.is_finished() for dependent_task in self.get_dependencies(task))
-
     def set_dependency(self, node_a, node_b):
         self.graph.add_edge(node_a, node_b)
-
-    def get_dependencies(self, node):
-        return self.graph.get_input_nodes(node)
-
-    def get_task_inputs(self, task):
-        return [dependent_task.result for dependent_task in self.get_dependencies(task) if dependent_task.result is not None]
-
-    def initial_check(self):
-        if not self.graph.is_acyclic():
-            raise ValueError("The graph has cycles. Cannot execute pipeline.")
 
     def run(self):
         self.initial_check()
@@ -41,6 +22,10 @@ class Pipeline:
             self.submit_ready_tasks()
             self.executor.wait_for_task_finish()
 
+    def initial_check(self):
+        if not self.graph.is_acyclic():
+            raise ValueError("The graph has cycles. Cannot execute pipeline.")
+
     def are_all_tasks_finished(self):
         return all(task.is_finished() for task in self.get_tasks())
 
@@ -48,3 +33,18 @@ class Pipeline:
         for task in self.get_ready_to_run_tasks():
             inputs = self.get_task_inputs(task)
             self.executor.submit_for_execution(task, inputs)
+
+    def get_ready_to_run_tasks(self):
+        return [task for task in self.get_tasks() if task.is_pending() and self.are_inputs_available(task)]
+
+    def get_tasks(self):
+        return self.graph.nodes
+
+    def are_inputs_available(self, task):
+        return all(dependent_task.is_finished() for dependent_task in self.get_dependencies(task))
+
+    def get_dependencies(self, node):
+        return self.graph.get_input_nodes(node)
+
+    def get_task_inputs(self, task):
+        return [dependent_task.result for dependent_task in self.get_dependencies(task) if dependent_task.result is not None]
